332ddca14ac19959a6c2fee46e7bbe27abf216bc
Dana <danamudrova@gmail.com>
2022-03-25 23:08:27 +0100

    feat: button, history of game, delay

--- sportka.js
+++ sportka.js
@@ -1,10 +1,17 @@
-// SPORTKA
+// SPORTKA - funkce pro klik na tlačítko (úkol 1)
 
+function losuj(){
+let textTlacitka = document.querySelector('#losuj')
 let osudi = [];
 let tazenaCisla = [];
 let vyherniCisla = document.querySelector('#vyherni-cisla');
+let historie = document.querySelector('#historie');
+ 
+document.querySelector('h1').textContent = "Výherní čísla";
+document.querySelector('h2').textContent = "Historie her";
+cisti();
 
-// generovani osudi
+// vytvoření osudi
 for (let i = 1; i <= 49; i = i + 1) {
     osudi.push(i);
 }
@@ -18,7 +25,24 @@ for (let i = 0; i < 7; i++) {
     osudi.splice(vyherniIndex, 1);
 }
 
-// vypis do HTML
+// výpis do HTML (tažení s 2sec zpožděním - úkol 2) a zápis historie (úkol 3)
 for (let j = 0; j < tazenaCisla.length; j++) {
-    vyherniCisla.innerHTML += '<span class="cislo">' + tazenaCisla[j] + '</span>';
+    (function(j){
+        window.setTimeout(function(){
+            vyherniCisla.innerHTML += '<span class="cislo">' + tazenaCisla[j] + '</span>';
+            historie.innerHTML += tazenaCisla [j]+", "
+        }, j*2000);
+    }(j));
 }
+
+textTlacitka.textContent = "Losuj znovu";
+historie.innerHTML += "<br>"
+
+}
+
+// vycisti formular pro dalsi losovani (úkol 3)
+function cisti(){
+    let vyherniCisla = document.querySelector('#vyherni-cisla');
+    vyherniCisla.innerHTML = ""
+}
+

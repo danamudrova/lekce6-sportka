2e00024c771257a8caa8dbbad76027bc18af04a1
Dana <danamudrova@gmail.com>
2022-03-26 22:16:24 +0100

    feat: graphical order of elements ( each game history own row, button position, order of numbers)

--- sportka.js
+++ sportka.js
@@ -1,43 +1,45 @@
 // SPORTKA - funkce pro klik na tlačítko (úkol 1)
 
 function losuj(){
-let textTlacitka = document.querySelector('#losuj')
-let osudi = [];
-let tazenaCisla = [];
-let vyherniCisla = document.querySelector('#vyherni-cisla');
-let historie = document.querySelector('#historie');
- 
-document.querySelector('h1').textContent = "Výherní čísla";
-document.querySelector('h2').textContent = "Historie her";
-cisti();
-
-// vytvoření osudi
-for (let i = 1; i <= 49; i = i + 1) {
-    osudi.push(i);
-}
-
-// z osudi vyberu 7 nahodnych cisel
-for (let i = 0; i < 7; i++) {
-    let vyherniIndex = Math.floor(Math.random() * osudi.length);
-    let vyherniCislo = osudi[vyherniIndex];
+    let osudi = [];
+    let tazenaCisla = [];
+    let vyherniCisla = document.querySelector('#vyherni-cisla');
+    let historie = document.querySelector('#historie');
+    let textTlacitka = document.querySelector('#losuj')
     
-    tazenaCisla.push(vyherniCislo);
-    osudi.splice(vyherniIndex, 1);
-}
+    document.querySelector('h1').textContent = "Výherní čísla";
+    document.querySelector('h2').textContent = "Historie her";
 
-// výpis do HTML (tažení s 2sec zpožděním - úkol 2) a zápis historie (úkol 3)
-for (let j = 0; j < tazenaCisla.length; j++) {
-    (function(j){
-        window.setTimeout(function(){
-            vyherniCisla.innerHTML += '<span class="cislo">' + tazenaCisla[j] + '</span>';
-            historie.innerHTML += tazenaCisla [j]+", "
-        }, j*2000);
-    }(j));
-}
+    cisti();
+ 
 
-textTlacitka.textContent = "Losuj znovu";
-historie.innerHTML += "<br>"
+    // vytvoření osudi
+    for (let i = 1; i <= 49; i = i + 1) {
+        osudi.unshift(i);
+    }
 
+    // z osudi vyberu 7 nahodnych cisel
+    for (let i = 0; i < 7; i++) {
+        let vyherniIndex = Math.floor(Math.random() * osudi.length);
+        let vyherniCislo = osudi[vyherniIndex];
+    
+        tazenaCisla.push(vyherniCislo);
+        osudi.splice(vyherniIndex, 1);
+    }
+
+    // výpis do HTML (tažení s 2sec zpožděním - úkol 2) a zápis historie (úkol 3)
+    for (let j = 0; j < tazenaCisla.length; j++) {
+        (function(j){
+            window.setTimeout(function(){
+                vyherniCisla.innerHTML += '<span class="cislo">' + tazenaCisla[j] + '</span>';
+                historie.innerHTML += tazenaCisla [j]+", "
+            }, j*2000);
+        }(j));
+    }
+
+    historie.innerHTML += "<br>"
+    textTlacitka.textContent = "Losuj znovu";
+    
 }
 
 // vycisti formular pro dalsi losovani (úkol 3)
@@ -45,4 +47,5 @@ function cisti(){
     let vyherniCisla = document.querySelector('#vyherni-cisla');
     vyherniCisla.innerHTML = ""
 }
+
 
